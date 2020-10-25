deeaea9ebf906bcdf9748fa0851786c73acfe5c3
Deepak Mahtani <deepak@hellopando.com>
2020-10-25 14:05:37 +0000

    updated annotations example file

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -39,4 +39,7 @@ const logNumber: (i :number) => void = (i: number) => {
 };
 
 // when to use annotations
-// 1) Function returns an anytype
+// 1) Function returns an 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json)
+console.log(coordinates); //{x: 10, y:20};
