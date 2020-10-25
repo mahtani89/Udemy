78d2a6cca9af4192a44cbbe0eb03214ef0397147
Deepak Mahtani <deepak@hellopando.com>
2020-10-25 14:34:00 +0000

    completed section 3/18

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -43,3 +43,26 @@ const logNumber: (i :number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coordinates: {x: number; y:number} = JSON.parse(json)
 console.log(coordinates); //{x: 10, y:20};
+
+// 2) when we declare a variable on one line and initilise it later
+let words = ['red', 'green', 'blue']
+
+let foundWord: boolean;
+
+for (let i = 0; i<words.length;i++) {
+	if (words[i]==='green') {
+		foundWord = true;
+	}
+}
+
+// 3) variable whose type that can't be inferred correctly
+
+let numbers =[ -10,-1,12]
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i< numbers.length; i++) {
+	if (numbers[i] > 0) {
+		numberAboveZero = numbers[i]
+	}
+}
+
