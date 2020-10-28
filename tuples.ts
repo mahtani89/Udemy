7b145f6c9f29e322b8aad223e89d0650a84b0d5b
Deepak Mahtani <deepak@hellopando.com>
2020-10-28 20:42:16 +0000

    completed section 6/18

---
+++ tuples.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+// eslint-disable-next-line no-unused-vars
+type Drink = [string, boolean, number]
+
+// eslint-disable-next-line no-unused-vars
+const pepsi: Drink = ['brown', true, 40];
+
+// eslint-disable-next-line no-unused-vars
+const sprite: Drink = ['brown', true, 40];
+
+// eslint-disable-next-line no-unused-vars
+const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3554];
+
+// eslint-disable-next-line no-unused-vars
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
